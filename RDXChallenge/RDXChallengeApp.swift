0a370c3f2b42845858858c18d65d74c2af59e945
Maciek Czarnik <czajnikowski@gmail.com>
2023-05-24 10:39:18 +0200

    Initial Commit

---
+++ RDXChallenge/RDXChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RDXChallengeApp.swift
+//  RDXChallenge
+//
+//  Created by Maciek on 24/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct RDXChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
